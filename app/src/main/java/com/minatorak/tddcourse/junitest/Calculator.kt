497ad5bebc93660e189484dac93519ed33cd858c
minatorak <hotmail22866@gmail.com>
2018-10-29 21:54:44 +0700

    refactor

--- app/src/main/java/com/minatorak/tddcourse/junitest/Calculator.kt
+++ app/src/main/java/com/minatorak/tddcourse/junitest/Calculator.kt
@@ -3,19 +3,19 @@ package com.minatorak.tddcourse.junitest
 class Calculator {
 
     fun add(i: Int, i1: Int): Int {
-        return 50
+        return i+i1
     }
 
     fun subtract(i: Int, i1: Int): Int {
-        return 10
+        return i-i1
     }
 
     fun multiply(i: Int, i1: Int): Int {
-        return 300
+        return i*i1
     }
 
     fun divide(i: Int, i1: Int): Int {
-        return 3
+        return i/i1
     }
 
 }
