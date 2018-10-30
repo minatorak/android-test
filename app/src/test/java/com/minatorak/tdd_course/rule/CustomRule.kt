3e8729235c98b05a3591f2492e37a193550e03b6
minatorak <hotmail22866@gmail.com>
2018-10-30 23:08:23 +0700

    create custom rule

--- app/src/test/java/com/minatorak/tdd_course/rule/CustomRule.kt
+++ app/src/test/java/com/minatorak/tdd_course/rule/CustomRule.kt
@@ -1,2 +1,23 @@
 package com.minatorak.tdd_course.rule
 
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+import java.util.logging.Logger
+
+class CustomRule : TestRule {
+
+    @JvmField
+    var logger: Logger? = null
+
+    fun getLogger(): Logger? {
+        return logger
+    }
+
+    override fun apply(base: Statement?, description: Description?): Statement? {
+
+        logger = Logger.getLogger(CustomRule::class.java.simpleName + ": this is my custom rule")
+        return null
+    }
+
+}
