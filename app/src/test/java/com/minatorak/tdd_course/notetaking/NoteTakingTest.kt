0a845994f5bbcaab88e71d4d79e4742524c05d20
minatorak <hotmail22866@gmail.com>
2018-10-30 21:13:23 +0700

    test suite

--- app/src/test/java/com/minatorak/tdd_course/notetaking/NoteTakingTest.kt
+++ app/src/test/java/com/minatorak/tdd_course/notetaking/NoteTakingTest.kt
@@ -1,2 +1,45 @@
 package com.minatorak.tdd_course.notetaking
 
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    NoteTakingTest.AddNoteText::class,
+    NoteTakingTest.AddImageNote::class,
+    NoteTakingTest.AddAudioNote::class
+)
+class NoteTakingTest {
+
+    class AddNoteText {
+        @Test
+        fun testEmptyNote() {
+
+        }
+
+        @Test
+        fun testMultilineText() {
+
+        }
+    }
+
+    class AddImageNote {
+        @Test
+        fun testGifImage() {
+
+        }
+
+        @Test
+        fun testVeryLargeImg() {
+
+        }
+    }
+
+    class AddAudioNote {
+        @Test
+        fun testMp3Audio() {
+
+        }
+    }
+}
