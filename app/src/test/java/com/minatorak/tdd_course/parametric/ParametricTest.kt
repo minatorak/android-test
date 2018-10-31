63b2b5933a1995ab4a54da6994b6bb573e3f2a52
minatorak <hotmail22866@gmail.com>
2018-10-31 16:55:55 +0700

    create class Parametric Test

--- app/src/test/java/com/minatorak/tdd_course/parametric/ParametricTest.kt
+++ app/src/test/java/com/minatorak/tdd_course/parametric/ParametricTest.kt
@@ -1,2 +1,17 @@
 package com.minatorak.tdd_course.parametric
 
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class ParametricTest {
+    private var numberA: Int? = null
+    private var numberB: Int? = null
+    private var expected: Int? = null
+
+    fun ParametricTest(a: Int, b: Int, expect: Int) {
+        numberA = a
+        numberB = b
+        expected = expect
+    }
+}
