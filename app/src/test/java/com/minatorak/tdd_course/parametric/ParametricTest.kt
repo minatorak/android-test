bc5705061c095cbe286b75cc4cd83cafbaebac5f
minatorak <hotmail22866@gmail.com>
2018-11-01 10:33:51 +0700

    create fun dataset

--- app/src/test/java/com/minatorak/tdd_course/parametric/ParametricTest.kt
+++ app/src/test/java/com/minatorak/tdd_course/parametric/ParametricTest.kt
@@ -2,6 +2,7 @@ package com.minatorak.tdd_course.parametric
 
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
+import java.util.*
 
 @RunWith(Parameterized::class)
 class ParametricTest {
@@ -13,5 +14,15 @@ class ParametricTest {
         numberA = a
         numberB = b
         expected = expect
+    }
+
+    companion object {
+
+        @Parameterized.Parameters
+        fun dataset(): Collection<Array<Any>> {
+            val data = arrayOf(arrayOf<Any>(1, 2, 3), arrayOf<Any>(2, 5, 7), arrayOf<Any>(10, 5, 9))
+            return Arrays.asList(*data)
+        }
+
     }
 }
