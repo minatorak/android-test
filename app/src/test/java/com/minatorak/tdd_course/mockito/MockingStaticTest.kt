e4c3ae344bf87c8016f1ff1f9742ecf49aec5556
minatorak <hotmail22866@gmail.com>
2018-11-01 21:42:02 +0700

    add function testPerson

--- app/src/test/java/com/minatorak/tdd_course/mockito/MockingStaticTest.kt
+++ app/src/test/java/com/minatorak/tdd_course/mockito/MockingStaticTest.kt
@@ -1,2 +1,11 @@
 package com.minatorak.tdd_course.mockito
 
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class MockingStaticTest{
+    @Test
+    fun testPerson() {
+        var person = mock(Person::class)
+    }
+}
