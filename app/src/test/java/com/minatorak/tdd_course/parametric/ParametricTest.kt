e6e86d00f9d0a9aae4638ecf96560892ed60246e
minatorak <hotmail22866@gmail.com>
2018-11-01 21:27:57 +0700

    parameter class test

--- app/src/test/java/com/minatorak/tdd_course/parametric/ParametricTest.kt
+++ app/src/test/java/com/minatorak/tdd_course/parametric/ParametricTest.kt
@@ -1,16 +1,20 @@
 package com.minatorak.tdd_course.parametric
 
+import com.minatorak.tddcourse.junitest.Calculator
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import java.util.*
 
 @RunWith(Parameterized::class)
 class ParametricTest {
-    private var numberA: Int? = null
-    private var numberB: Int? = null
-    private var expected: Int? = null
 
-    fun ParametricTest(a: Int, b: Int, expect: Int) {
+    private var numberA: Int = 1
+    private var numberB: Int = 2
+    private var expected: Int = 3
+
+    constructor(a: Int, b: Int, expect: Int) {
         numberA = a
         numberB = b
         expected = expect
@@ -23,6 +27,11 @@ class ParametricTest {
             val data = arrayOf(arrayOf<Any>(1, 2, 3), arrayOf<Any>(2, 5, 7), arrayOf<Any>(10, 5, 9))
             return Arrays.asList(*data)
         }
+    }
 
+    @Test
+    fun testAddingTwoPositiveNumber() {
+        val calculator = Calculator()
+        assertEquals(expected,calculator.add(numberA,numberB))
     }
 }
