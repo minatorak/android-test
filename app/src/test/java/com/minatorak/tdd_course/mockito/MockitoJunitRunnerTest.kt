1db3919cd58cb495ea713b99ea264f55172c3668
minatorak <hotmail22866@gmail.com>
2018-11-01 21:59:20 +0700

    class mockking runner

--- app/src/test/java/com/minatorak/tdd_course/mockito/MockitoJunitRunnerTest.kt
+++ app/src/test/java/com/minatorak/tdd_course/mockito/MockitoJunitRunnerTest.kt
@@ -1,2 +1,14 @@
 package com.minatorak.tdd_course.mockito
 
+import com.minatorak.tddcourse.mockito.Person
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class MockitoJunitRunnerTest {
+
+    @Mock
+    private lateinit var person: Person
+
+}
