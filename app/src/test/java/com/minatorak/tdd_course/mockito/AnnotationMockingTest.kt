84e366c1cf3727b7373323e83d52efa9c1c78486
minatorak <hotmail22866@gmail.com>
2018-11-01 21:54:51 +0700

    define object

--- app/src/test/java/com/minatorak/tdd_course/mockito/AnnotationMockingTest.kt
+++ app/src/test/java/com/minatorak/tdd_course/mockito/AnnotationMockingTest.kt
@@ -1,10 +1,17 @@
 package com.minatorak.tdd_course.mockito
 
 import com.minatorak.tddcourse.mockito.Person
+import org.junit.Before
 import org.mockito.Mock
+import org.mockito.MockitoAnnotations
 
 class AnnotationMockingTest {
 
     @Mock
     private lateinit var person: Person
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+    }
 }
