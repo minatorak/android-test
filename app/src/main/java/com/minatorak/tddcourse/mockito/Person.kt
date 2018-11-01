513d66990b5caf7ca6d7f3148de2c0cb86847cdb
minatorak <hotmail22866@gmail.com>
2018-11-01 21:45:48 +0700

    mock class Person

--- app/src/main/java/com/minatorak/tddcourse/mockito/Person.kt
+++ app/src/main/java/com/minatorak/tddcourse/mockito/Person.kt
@@ -1,2 +1,5 @@
 package com.minatorak.tddcourse.mockito
 
+class Person {
+
+}
--- app/src/test/java/com/minatorak/tdd_course/mockito/MockingStaticTest.kt
+++ app/src/test/java/com/minatorak/tdd_course/mockito/MockingStaticTest.kt
@@ -1,11 +1,12 @@
 package com.minatorak.tdd_course.mockito
 
+import com.minatorak.tddcourse.mockito.Person
 import org.junit.Test
 import org.mockito.Mockito.mock
 
 class MockingStaticTest{
     @Test
     fun testPerson() {
-        var person = mock(Person::class)
+        var person = mock(Person::class.java)
     }
 }
