ab7700009f621d9ce31c0d717072e2e56609b8ce
minatorak <hotmail22866@gmail.com>
2018-11-01 21:53:19 +0700

    mock Person()

--- app/src/test/java/com/minatorak/tdd_course/mockito/AnnotationMockingTest.kt
+++ app/src/test/java/com/minatorak/tdd_course/mockito/AnnotationMockingTest.kt
@@ -1,2 +1,10 @@
 package com.minatorak.tdd_course.mockito
 
+import com.minatorak.tddcourse.mockito.Person
+import org.mockito.Mock
+
+class AnnotationMockingTest {
+
+    @Mock
+    private lateinit var person: Person
+}
