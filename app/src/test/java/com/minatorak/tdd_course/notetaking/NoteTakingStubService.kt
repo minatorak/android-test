35c8d4863e55516b61840c39d61256517011f2a1
minatorak <hotmail22866@gmail.com>
2018-11-02 21:42:45 +0700

    stub class for NoteTakingService

---
+++ app/src/test/java/com/minatorak/tdd_course/notetaking/NoteTakingStubService.kt
@@ -0,0 +1,11 @@
+package com.minatorak.tdd_course.notetaking
+
+import com.minatorak.tddcourse.junitest.NoteTakingService
+
+class NoteTakingStubService : NoteTakingService {
+
+    override fun getAllBusinessNotes(): List<String> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
