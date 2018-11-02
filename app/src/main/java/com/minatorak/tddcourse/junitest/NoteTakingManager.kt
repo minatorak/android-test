f84896dd0187513ea39cf318d74d31714f2b7c5d
minatorak <hotmail22866@gmail.com>
2018-11-02 22:37:28 +0700

    function check note text

--- app/src/main/java/com/minatorak/tddcourse/junitest/NoteTakingManager.kt
+++ app/src/main/java/com/minatorak/tddcourse/junitest/NoteTakingManager.kt
@@ -1,10 +1,20 @@
 package com.minatorak.tddcourse.junitest
 
-class NoteTakingManager(var service: NoteTakingService) {
+class NoteTakingManager(private var service: NoteTakingService) {
 
     fun getAllBusinessNotes(): List<String>? {
-        return listOf("Note 1","Note 2")
+        val filter = mutableListOf<String>()
+        val allNote = service.getAllBusinessNotes()
+
+        for (note in allNote) {
+            if (note.contains("Android")) {
+                filter.add(note)
+            }
+        }
+
+        return filter
     }
+
 
 
 }
