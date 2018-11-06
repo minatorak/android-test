54df1fcdb3e86c71e4676c0bf484d7cb2e0af244
minatorak <hotmail22866@gmail.com>
2018-11-06 16:51:30 +0700

    Mockito Test on BusinessNotes

--- app/src/test/java/com/minatorak/tdd_course/notetaking/BusinessNotesTest.kt
+++ app/src/test/java/com/minatorak/tdd_course/notetaking/BusinessNotesTest.kt
@@ -1,14 +1,28 @@
 package com.minatorak.tdd_course.notetaking
 
 import com.minatorak.tddcourse.junitest.NoteTakingManager
+import com.minatorak.tddcourse.junitest.NoteTakingService
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
 
 class BusinessNotesTest {
+
     @Test
     fun testRetrieveBusinessNotes() {
         val service = NoteTakingStubService()
         val manager = NoteTakingManager(service)
         assertEquals(2, manager.getAllBusinessNotes().size)
+    }
+
+    @Test
+    fun mockitoRetrieveBusinessNotes() {
+        val service: NoteTakingService = mock(NoteTakingService::class.java)
+        val data = listOf("Android make", "Android is awesome", "I am Super")
+        `when`(service.getAllBusinessNotes()).thenReturn(data)
+
+        val manager = NoteTakingManager(service)
+        assertEquals(2,manager.getAllBusinessNotes().size)
     }
 }
