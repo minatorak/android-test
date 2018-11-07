23c3285d595e335d34c873b30c0b7a86cfa10e4a
minatorak <hotmail22866@gmail.com>
2018-11-07 17:42:54 +0700

    create activity Login

---
+++ app/src/main/java/com/minatorak/tddcourse/LoginScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.minatorak.tddcourse
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+    }
+}
